9bbbdad6a95927489fc6336b19c891b861b3e5b8
wanghy <840203856@qq.com>
2018-11-22 16:55:31 +0800

    这个波浪呀,完全不按套路出牌.

--- app/src/main/java/com/bwie/customerview/MainActivity.java
+++ app/src/main/java/com/bwie/customerview/MainActivity.java
@@ -1,20 +1,38 @@
 package com.bwie.customerview;
 
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImage;
+    private WaveView mWaveView;
+    private FrameLayout.LayoutParams flp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
 
 
+    private void initView() {
+        mImage = findViewById(R.id.image);
+        mWaveView = findViewById(R.id.waveView);
+
+        flp = new FrameLayout.LayoutParams(-2, -2);
+        flp.gravity = Gravity.CENTER;
+
+        mWaveView.setAnimations(new WaveView.Animations() {
+            @Override
+            public void getY(float y) {
+                flp.setMargins(0, -20, 0, (int) (y + 2));
+                mImage.setLayoutParams(flp);
+            }
+        });
+    }
 }
